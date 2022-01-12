b539535aa60f753028fe86b4cdb0a1eda2c991fe
hyewon-p <hyewon.parkhw@gmail.com>
2022-01-12 16:33:15 +0900

    Coin tracker

--- src/App.js
+++ src/App.js
@@ -1,36 +1,49 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-
-    setToDos((currentArray) => [...currentArray, toDo]);
-    setToDo("");
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [BTC, setBTC] = useState("");
+  const [selected, setSelected] = useState("");
+  const handleSelect = (event) => {
+    setSelected(event.target.value);
   };
+  const onChange = (event) => {
+    setBTC(event.target.value);
+  };
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers").then((response) =>
+      response.json().then((json) => {
+        setCoins(json);
+        setLoading(false);
+      })
+    );
+  }, []);
   return (
     <div>
-      <h1>My To Dos({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((todo, index) => (
-          <li key={index}>{todo}</li>
-        ))}
-      </ul>
+      <h1>The Coins! ({coins.length})</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <div>
+          <div>
+            <label htmlFor="money">How many coins can I get with: </label>
+            <input name="money" value={BTC} onChange={onChange}></input>
+          </div>
+          <select onChange={handleSelect} value={selected}>
+            {coins.map((coin) => (
+              <option>
+                {coin.name}({coin.symbol}): {coin.quotes.USD.price} USD
+              </option>
+            ))}
+          </select>
+          {selected === "" ? null : (
+            <h2>
+              {parseInt(BTC / parseFloat(selected.split(":")[1].split(" USD")))}
+            </h2>
+          )}
+        </div>
+      )}
     </div>
   );
 }
